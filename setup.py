ae553c769da6bb8e6602dcf459db94d44d9ad7ce
Jay <jlaura@asu.edu>
2021-07-13 06:25:13 -0700

    ticket minor minor on URL fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as f:
 def setup_package():
     setup(
         name = "amg",
-        version = '0.2.0',
+        version = '0.2.1',
         author = "Jay Laura",
         author_email = "jlaura@usgs.gov",
         description = ("Automated metadata generation"),
